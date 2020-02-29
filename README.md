9fafd71f97f47e4a1b0aa7838d4ff58fe632beb9
Evan Morisato <evanmorisato@yahoo.com>
2020-02-29 15:09:00 -0600

    Done

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # friend-finder
 Compatibility Based Friend Finder Application
+
+Friend Finder is used by entering user name and adding a photo. Then the user will answer 10 questions. The answers to these questions will be compared with the answers of other users. The user which has the smallest difference from your score will be matched as the most compatible with you!
+
+Technologies Used:
+    -JavaScript
+    -jQuery
+    -node.js
+    -express.js
+    -HTML
+    -Bootstrap
---
+++ app/data/friends.js
@@ -0,0 +1,34 @@
+var friends = [
+    {
+        name: "Stephen Curry",
+        photo: "https://pbs.twimg.com/profile_images/709000642874839040/21UqEdVS_400x400.jpg",
+        scores: [1, 5, 4, 3, 3, 5, 4, 2, 1, 5]
+    }, 
+    {
+        name: "BJ Penn",
+        photo: "https://gossipgist.com/uploads/27275/bj-penn-forumssherdog.jpg",
+        scores: [4, 5, 3, 3, 2, 1, 1, 1, 4, 5]
+    }, 
+    {
+        name: "Max Holloway",
+        photo: "https://www.gameonlivesports.com.au/TeamLogo/HOLLOWAY_MAX.png",
+        scores: [3, 3, 2, 3, 1, 5, 4, 1, 2, 2]
+    }, 
+    {
+        name: "Matt Chapman",
+        photo: "https://www.mercurynews.com/wp-content/uploads/2018/02/bng-l-mchampan-8.jpg?w=412",
+        scores: [5, 4, 5, 3, 2, 1, 4, 2, 1, 3]
+    }, 
+    {
+        name: "Derek Carr",
+        photo: "https://www.daddyleagues.com/img/nfl/madden17/6374.png",
+        scores: [4, 2, 5, 1, 3, 4, 2, 1, 5, 5]
+    }, 
+    {
+        name: "Valentina Shevchenko",
+        photo: "https://res-2.cloudinary.com/crunchbase-production/image/upload/c_thumb,h_256,w_256,f_auto,g_faces,z_0.7,q_auto:eco/tafwmyfkbma6gxc8q5mo",
+        scores: [3, 2, 1, 5, 3, 1, 4, 4, 1, 1]
+    }, 
+];
+
+module.exports = friends;
